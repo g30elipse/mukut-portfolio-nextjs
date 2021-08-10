fbca9d9f22d6bc9d62116b1c1854f75f062bdf13
Mukut Brahma <mukutb.mac@gmail.com>
2021-08-10 23:51:07 +0530

    created tsconfig; updated headers; updated readme

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Index({ allPosts }) {
     <>
       <Layout>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>Mukut Brahma - Portfolio</title>
         </Head>
         <Container>
           <Intro />
